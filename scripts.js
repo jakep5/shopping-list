4d778b126a75fe4e7c4fa290c7edffc1ec06003d
jakep5 <jakepagel1@yahoo.com>
2019-09-17 12:22:18 -0500

    created scripts.js and modified index.html

---
+++ scripts.js
@@ -0,0 +1,22 @@
+$("form :button").on("click", function(){
+        event.preventDefault();
+        let item = document.getElementById("shopping-list-entry").value;
+        let checkButton = "check";
+        let deleteButton = "delete";
+        $(".shopping-list").append([
+            ("<li><span class='shopping-item'>"+ item +"</span></li>"),
+            ("<div class='shopping-item-controls'><button class='shopping-item-toggle'><span class='button-label'>" + checkButton + "</span></button><button class='shopping-item-delete'><span class='button-label'>" + deleteButton + "</span></button></div></li>"),
+        ]);
+    });
+
+//$("button.shopping-item-toggle").on("click", function(){
+    //$(".shopping-item").attr(".shopping-item", ".shopping-item__checked");
+//});
+
+$("button.shopping-item-toggle").on("click", function(){
+    $(this).parents().siblings(".shopping-item").toggleClass("shopping-item__checked");
+});
+
+$("button.shopping-item-delete").on("click", function(){
+    $(this).parents().eq(1).remove();
+});
