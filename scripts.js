f579908580bbb7d366de2532dfb5cfab84cd94a2
jakep5 <jakepagel1@yahoo.com>
2019-09-17 16:16:13 -0500

    finished JQuery functionality

--- scripts.js
+++ scripts.js
@@ -1,27 +1,26 @@
-$("form :button").on("click", function(){
-        event.preventDefault();
-        let item = document.getElementById("shopping-list-entry").value;
-        let checkButton = 'check';
-        let deleteButton = 'delete';
-        $(".shopping-list").append(
-            <li>\
-            <span class='shopping-item'>+ item +</span>\
-            <div class='shopping-item-controls'>\
-                <button class='shopping-item-toggle'>\
-                    <span class='button-label'>+ checkButton +</span>\
-                </button>\
-                <button class='shopping-item-delete'>\
-                    <span class='button-label'>+ deleteButton +</span>\
-                </button>\
-                </div>\
-            </li>
-        )});
-        
+$(function(){
+    $("form :button").on("click", function(){
+            event.preventDefault();
+            let item = document.getElementById("shopping-list-entry").value;
+            $(".shopping-list").append(`
+                <li>
+                <span class='shopping-item'>${item}</span>
+                <div class='shopping-item-controls'>
+                    <button class='shopping-item-toggle'>
+                        <span class='button-label'>check</span>
+                    </button>
+                    <button class='shopping-item-delete'>
+                        <span class='button-label'>delete</span>
+                    </button>
+                    </div>
+                </li>
+            `)});
+            
+    $("ul").on("click", ".shopping-item-toggle", function() {
+        $(this).parents().siblings(".shopping-item").toggleClass("shopping-item__checked");
+    });
 
-$("button.shopping-item-toggle").on("click", function(){
-    $(this).parents().siblings(".shopping-item").toggleClass("shopping-item__checked");
-});
-
-$("button.shopping-item-delete").on("click", function(){
-    $(this).parents().eq(1).remove();
-});
+    $("ul").on("click", ".shopping-item-delete", function() {
+        $(this).parents().eq(1).remove();
+    });
+})
